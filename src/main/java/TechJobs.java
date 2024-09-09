305fb275b981268cd739a439b83269849e2446c7
Doug Hirlinger <doughirlinger@gmail.com>
2024-09-09 12:48:57 -0400

    task 1 complete

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -62,7 +62,8 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    printJobs(JobData.findByValue(searchTerm));
+
+                printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -120,6 +121,17 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (!someJobs.isEmpty()) {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> term : job.entrySet()) {
+                    System.out.println(term.getKey() + ": " + term.getValue());
+                }
+                System.out.println("*****");
+            }
+
+        } else {
+            System.out.println("No Results");
+        }
     }
 }
